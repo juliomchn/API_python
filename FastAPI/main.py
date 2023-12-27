cb7da62ceeca1f4cbfa9aa32793c8c0c5fa052f3
Julio Machin <litonfire1@gmail.com>
2023-12-27 12:28:03 +0100

    adding routers and moving methods

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,93 +1,18 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import Union
+from routers import films
 
+
+#Start server
 #python -m uvicorn main:app --reload
+
+#Documentation
+#http://127.0.0.1:8000/docs
 
 app = FastAPI()
 
-class Film(BaseModel):
-    id: int
-    titulo: str
-    director: str
-    puntuacion: float
-    nacionalidad: str
-
-films_list = [Film(id=1,titulo= "The First Slam Dunk (2022)",director= "Takehiko Inoue",puntuacion= 7.8, nacionalidad= "Japón" ),
-              Film(id=2, titulo= "As bestas (2022)", director="Rodrigo Sorogoyen",puntuacion= 7.6,nacionalidad= "España"),
-              Film(id=3, titulo="El regreso de las golondrinas (2022)",director="Li Ruijun", puntuacion= 7.6, nacionalidad="China")]
-
-
-
-
-@app.get("/usersjson")
-async def peliculasjson():
-    return[{
-      "id": "1",
-      "titulo": "The First Slam Dunk (2022)",
-      "director": "Takehiko Inoue",
-      "puntuacion": 7.8,
-      "nacionalidad": "Japón"
-    },
-    {
-      "id": "2",
-      "titulo": "As bestas (2022)",
-      "director": "Rodrigo Sorogoyen",
-      "puntuacion": 7.6,
-      "nacionalidad": "España"
-    },
-    {
-      "id": "3",
-      "titulo": "El regreso de las golondrinas (2022)",
-      "director": "Li Ruijun",
-      "puntuacion": 7.6,
-      "nacionalidad": "China"
-    }]
+app.include_router(films.router)
 
 
 @app.get("/")
 def read_root():
     return {"API de PELICULAS"}
-
-@app.get("/films")
-async def films ():
-    return films_list
-
-@app.get("/films/")
-async def films (id: int):
-    return search_films
-
-@app.get("/films/{id}")
-def search_films(id: int):
-    return search_films(id)
-
-@app.get("/filmsquery/")
-async def films(id: int):
-    return search_films(id)
-
-@app.post("/addfilm/")
-async def add_film(newFilm: Film):
-    if type(search_films(newFilm.id)) == Film:
-        return { "error": "This film arlready exists"}
-    else:
-        add_film(newFilm)
-
-@app.put("/updatefilm/")
-async def update_film(newFilm: Film):
-    update_film(newFilm)
-
-
-def update_film(updateFilm: Film):
-
-    films_list[updateFilm.id - 1 ] = updateFilm
-
-
-def search_films(id: int):
-    films = filter(lambda film: film.id == id, films_list)
-    try:
-        return list(films)[0]
-    except:
-        return { "error": "Not found this film"}
-
-def add_film(newFilm: Film):
-    films_list.append(newFilm)
---
+++ FastAPI/routers/films.py
@@ -0,0 +1,111 @@
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
+from typing import Union
+
+router = APIRouter()
+
+class Film(BaseModel):
+    id: int
+    titulo: str
+    director: str
+    puntuacion: float
+    nacionalidad: str
+
+films_list = [Film(id=1,titulo= "The First Slam Dunk (2022)",director= "Takehiko Inoue",puntuacion= 7.8, nacionalidad= "Japón" ),
+              Film(id=2, titulo= "As bestas (2022)", director="Rodrigo Sorogoyen",puntuacion= 7.6,nacionalidad= "España"),
+              Film(id=3, titulo="El regreso de las golondrinas (2022)",director="Li Ruijun", puntuacion= 7.6, nacionalidad="China")]
+
+
+
+
+@router.get("/usersjson")
+async def peliculasjson():
+    return[{
+      "id": "1",
+      "titulo": "The First Slam Dunk (2022)",
+      "director": "Takehiko Inoue",
+      "puntuacion": 7.8,
+      "nacionalidad": "Japón"
+    },
+    {
+      "id": "2",
+      "titulo": "As bestas (2022)",
+      "director": "Rodrigo Sorogoyen",
+      "puntuacion": 7.6,
+      "nacionalidad": "España"
+    },
+    {
+      "id": "3",
+      "titulo": "El regreso de las golondrinas (2022)",
+      "director": "Li Ruijun",
+      "puntuacion": 7.6,
+      "nacionalidad": "China"
+    }]
+
+
+@router.get("/films")
+async def films ():
+    return films_list
+
+@router.get("/films/")
+async def films (id: int):
+    return search_films
+
+@router.get("/films/{id}")
+def search_films(id: int):
+    return search_films(id)
+
+@router.get("/filmsquery/")
+async def films(id: int):
+    return search_films(id)
+
+@router.post("/addfilm/",response_model= Film ,status_code=201)
+async def add_film(newFilm: Film):
+    if type(search_films(newFilm.id)) == Film:
+        raise HTTPException(status_code=404, detail="This film already exists")
+    else:
+        return add_film(newFilm)
+
+@router.put("/updatefilm/")
+async def update_film(newFilm: Film):
+    return update_film(newFilm)
+
+@router.delete("/deletefilm/{id}")
+async def delete_film(id: int):
+    return delete_film(id)
+
+
+def update_film(updateFilm: Film): 
+    for i, f in enumerate(films_list):
+        if f.id == updateFilm.id:
+            films_list[i] = updateFilm
+            found = True
+
+    if not found:
+        return { "error": "Not found this film"}
+    else:
+        return updateFilm
+    
+def delete_film(id: int):
+    for i, f in enumerate(films_list):
+        if f.id == id:
+            del films_list[i]
+            found = True
+        
+    if not found: 
+       return { "error": "Not found this film"}
+    else:
+        return { "Deleted film succesfully!"}
+
+
+
+def search_films(id: int):
+    films = filter(lambda film: film.id == id, films_list)
+    try:
+        return list(films)[0]
+    except:
+        return { "error": "Not found this film"}
+
+def add_film(newFilm: Film):
+    films_list.append(newFilm)
+    return newFilm
