ac6c16932dac3bbacb07e649839dd05ea3f82eb5
Julio Machin <litonfire1@gmail.com>
2024-01-02 17:24:34 +0100

    Added new json films to test

--- FastAPI/db/dbConnection.py
+++ FastAPI/db/dbConnection.py
@@ -1,3 +1,6 @@
 from pymongo import MongoClient
 
+# Run DataBase in local
+#"D:\Program Files\MongoDB\Server\7.0\bin\mongod.exe" --dbpath="D:\Users\50051512\Julio\MongoDB"
+
 db_connection = MongoClient().local
